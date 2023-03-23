19884b0ea58ad61119aaae04302410256070adfd
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2023-03-23 01:45:53 -0700

    center

--- src/components/StyledHeader.tsx
+++ src/components/StyledHeader.tsx
@@ -14,7 +14,7 @@ export function StyledHeader({...otherProps}: Header1Props) {
     return (
         <div className={`flex w-full justify-center`}>
             <div className={``}>
-                <h1 className={`${rowdies.className} text-5xl md:text-[5.252rem]`} {...otherProps} />
+                <h1 className={`${rowdies.className} text-center text-5xl md:text-[5.252rem]`} {...otherProps} />
                 <ThreeDots containerClassName={`relative bottom-3 md:bottom-5 right-5 md:right-7 -z-10`} />
             </div>
         </div>
