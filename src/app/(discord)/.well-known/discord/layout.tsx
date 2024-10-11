815c3d80587be62b516eac4bd319231a0801f7d6
Ilia <eilia.abedian@yahoo.com>
2024-10-10 20:27:27 -0700

    disocrd

--- src/app/(discord)/.well-known/discord/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export default function DiscordLayout({ children }: PropsWithChildren) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  );
-}
--- src/app/(discord)/.well-known/discord/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DiscordDomainVerification() {
-  return 'dh=89f1b7159e02e342e5aa8b25258a1abd9815749e';
-}
